9c4208742c6553461319d88f10a4cb3fe2b29dd7
NaBiiiiiL <ithelp1378@gmail.com>
2023-06-23 02:31:22 -0700

    last

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -24,7 +24,8 @@ double _sqrt(double x)
 	return (sqrt);
 }
 /**
- * largest_print_factor - finds and prints the largest primr factor of number (num)
+ * largest_print_factor - finds and prints the largest
+ * primr factor of number (num)
  *
  * @num: number to find
  */
@@ -33,9 +34,11 @@ void largest_print_factor(long int num)
 {
 	int prmNu, largest;
 
+	/* first divide with the smallest prime number (two) */
 	while (num % 2 == 0)
 		num = num / 2;
 
+	/* num must be odd so we proceed to the next prime number (plus toe) */
 	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
 	{
 		while (num % prmNu == 0)
@@ -47,8 +50,14 @@ void largest_print_factor(long int num)
 
 	if (num > 2)
 		largest = num;
-		printf("%d\n", largest);
+	printf("%d\n", largest);
 }
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
