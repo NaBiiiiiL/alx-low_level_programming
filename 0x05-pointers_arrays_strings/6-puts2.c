dbaf768ad9a5a205ca80f05f19c13adba6fb48e9
NaBiiiiiL <ithelp1378@gmail.com>
2023-06-26 05:28:22 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,6 +3,8 @@
 /**
  * puts2 - prints every character of a string
  *
+ * @str: string parameter input
+ *
  * Return: Nothing
  */
 
@@ -12,7 +14,7 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; ++i)
 	{
-		if (i %2 == 0)
+		if (i % 2 == 0)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
