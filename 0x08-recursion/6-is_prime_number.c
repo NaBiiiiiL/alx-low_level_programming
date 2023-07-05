118a91da1a65ceeb18b15a77113c8f3989b8e6c4
NaBiiiiiL <ithelp1378@gmail.com>
2023-07-05 13:21:57 -0700

    100-is_palindrome.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 int actual_prime(int n, int i);
 
 /**
- * is_prime_number - says if an integer is a print number or not
+ * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
  *
  * Return: 1 if n is a prime number, 0 if not
@@ -23,12 +23,13 @@ int is_prime_number(int n)
  *
  * Return: 1 if n is prime, 0 if not
  */
-
-int actual_prime(int n, int i)
+int actual_prime(int  n, int i)
 {
 	if (i == 1)
-		 return (1);
+		return (1);
+
 	if (n % i == 0 && i > 0)
-		 return (0);
+		return (0);
+
 	return (actual_prime(n, i - 1));
 }
