0b325df25cf392cba39adb2d234ba912a48a3efa
NaBiiiiiL <ithelp1378@gmail.com>
2023-07-07 14:28:32 -0700

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function
+ *
+ * @n: takes in integer type
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
