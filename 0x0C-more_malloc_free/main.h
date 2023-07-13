32188fd4a31431b5835346dbe84e9104ad39012b
NaBiiiiiL <ithelp1378@gmail.com>
2023-07-13 15:50:27 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,5 +12,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
+int main(int argc, char const *argv[]);
 
 #endif
