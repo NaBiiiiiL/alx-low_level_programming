8eae94d4fd873d6c6f9314c396e9311301780d52
NaBiiiiiL <ithelp1378@gmail.com>
2023-07-19 10:29:01 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -18,10 +18,10 @@ int main(char argc, char **argv)
 	if (argc != 2)
 		printf("Error\n"), exit(1);
 	b = atoi(argv[1]);
-	if ( b < 0)
+	if (b < 0)
 		printf("Error\n"), exit(2);
 
 	while (b--)
-		printf("%02hhx%s', *p++, b ? " " : "\n");
+		printf("%02hhx%s", *p++, b ? " " : "\n");
 	return (0);
 }
